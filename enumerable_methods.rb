d56adf7cd524382c3ec2578da25c0aa00a11bb25
Cody Hacker <jbalsamo@cox.net>
2015-06-07 14:48:47 -0700

    everything works with ranges now

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -5,7 +5,7 @@ module Enumerable
       def my_each
       	i = 0
       	while i < self.size
-      		yield(self[i])
+      		yield(self.to_a[i])
       		i += 1
       	end
       end
@@ -13,19 +13,19 @@ module Enumerable
       def my_each_with_index
       	i = 0
       	while i < self.size
-      		yield(self[i], i)
+      		yield(self.to_a[i], i)
       		i += 1
       	end
       end
 
       def my_select
       	return_array = []
-      	self.my_each {|num| return_array << num if yield(num)}
+      	self.to_a.my_each {|num| return_array << num if yield(num)}
       	return_array
       end
 
       def my_all?
-      	my_each do |element|
+      	self.to_a.my_each do |element|
       		unless yield(element) 
       			return false
       		end
@@ -34,7 +34,7 @@ module Enumerable
       end
 
       def my_any?
-      	my_each do |element|
+      	self.my_each do |element|
       		if yield(element)
       			return true
       		end
@@ -43,7 +43,7 @@ module Enumerable
       end
 
       def my_none?
-      	my_each do |element|
+      	self.my_each do |element|
       		if yield(element)
       			return false
       		end
@@ -54,9 +54,9 @@ module Enumerable
       def my_count(item_to_count = nil)
       	items_counted = 0
       	if item_to_count
-      		self.my_each { |item| items_counted += 1 if item == item_to_count }
+      		self.to_a.my_each { |item| items_counted += 1 if item == item_to_count }
       	elsif block_given?
-      		self.my_each { |item| items_counted += 1 if yield(item) }
+      		self.to_a.my_each { |item| items_counted += 1 if yield(item) }
       	else
       		items_counted = self.size
       	end
@@ -65,14 +65,22 @@ module Enumerable
 
       def my_map
       	return_array = []
-      	my_each {|element| return_array << yield(element)}
+      	self.to_a.my_each {|element| return_array << yield(element)}
       	return_array
+      end
+
+      def my_inject
+      	sum = 0
+      	self.to_a.my_each {|element| sum = yield(sum, element)}
+      	return sum
       end
 
 end
 
 test_array = [1,2,3,4,5,3]
 test_array.my_each {|x| puts x * 10}
+puts " "
+(5..10).my_each {|x| puts x * 10}
 test_array.my_each_with_index {|x, y| puts "Item: #{x}, Item Index: #{y}"}
 p test_array.my_select {|num| num.even?}
 p test_array.my_all? {|num| num.even?}
@@ -81,5 +89,8 @@ p test_array.my_none? {|num| num == 3}
 p test_array.my_count
 p test_array.my_count(3)
 p test_array.my_count {|num| num >= 3}
-p test_array.my_map {|x| "#{x} * 3 = #{3 * x}"}
-
+p test_array.my_map {|x| "#{x} * 3 = #{3 * x}"} 
+p (5..10).my_map {|x| "#{x} * 3 = #{3 * x}"} 
+p test_array.my_inject { |sum, n| sum + n } 
+p (5..10).my_inject { |sum, n| sum + n } 
+p (5..10).inject { |sum, n| sum + n } 
