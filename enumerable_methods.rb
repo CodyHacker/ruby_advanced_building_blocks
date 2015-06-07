1a4a781d3501aa6ecefdc0f98029f7faf611c336
Cody Hacker <jbalsamo@cox.net>
2015-06-07 10:38:42 -0700

    #my_each_with_index method

--- enumerable_methods.rb
+++ enumerable_methods.rb
@@ -8,9 +8,18 @@ module Enumerable
       		yield(self[i])
       		i += 1
       	end
-      	
+      end
+
+      def my_each_with_index
+      	i = 0
+      	while i < self.size
+      		yield(self[i], i)
+      		i += 1
+      	end
       end
 
 end
 
 test_array.my_each {|x| puts x * 10}
+
+test_array.my_each_with_index {|x, y| puts "Item: #{x}, Item Index: #{y}"}
